2b3bbfb4b9f401d103e037a5554a8adbb2cfb9b0
tarplmic <michaela.tarpley@gmail.com>
2021-04-04 02:38:26 -0500

    Copied example code for recieving data from ground xbee.

---
+++ samd21ExampleCodes/samd21_readingSerialData_firstTest/samd21_readingSerialData_firstTest.ino
@@ -0,0 +1,57 @@
+
+// Example 3 - Receive with start- and end-markers
+
+const byte numChars = 32;
+char receivedChars[numChars];
+
+boolean newData = false;
+
+void setup() {
+    Serial1.begin(9600);
+    Serial1.println("<Arduino is ready>");
+}
+
+void loop() {
+    recvWithStartEndMarkers();
+    showNewData();
+}
+
+void recvWithStartEndMarkers() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+ 
+    while (Serial1.available() > 0 && newData == false) {
+        rc = Serial1.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+void showNewData() {
+    if (newData == true) {
+        Serial1.print("This just in ... ");
+        Serial1.println(receivedChars);
+        newData = false;
+    }
+}
